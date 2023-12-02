307bf5c89ba999da25d45e7879275ab1e89ce995
epicisgood <144903320+epicisgood@users.noreply.github.com>
2023-12-01 20:31:22 -0500

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands, bridge
 from googleapiclient.discovery import build
-
+from main import GOOGLE_API
 
 class Google_Search(commands.Cog):
   def __init__(self, bot):
@@ -9,7 +9,7 @@ class Google_Search(commands.Cog):
   @bridge.bridge_command(name='google_search',with_app_command=True, aliases=['google', 'search'])
   async def google_search(self, ctx, *, search_inp: str):
     google_sevice = build(
-        "customsearch", "v1", developerKey=""
+        "customsearch", "v1", developerKey=GOOGLE_API
     )
     
     
