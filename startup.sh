35e133fe8f2a7429f8ec705dfe9cb341be50abb7
epicisgood <144903320+epicisgood@users.noreply.github.com>
2023-12-04 19:11:24 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,8 +3,4 @@ pip install openai==0.28
 pip install py-cord
 pip install colorama
 pip install google-api-python-client
-git clone https://github.com/epicisgood/nitrogpt
-cd nitrogpt
-git fetch
-git pull
 python main.py
